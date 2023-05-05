80d25a8ed3e129917d28e41f776a476eb2d0f09f
asape67 <128886513+asape67@users.noreply.github.com>
2023-05-05 16:35:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
+@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -21,15 +21,14 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
+data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram 
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]()
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
